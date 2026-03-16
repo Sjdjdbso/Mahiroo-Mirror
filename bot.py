9e84b86b97dbf5273932f35c2a0eadb50ea65b79
Sjdjdbso <168467152+Sjdjdbso@users.noreply.github.com>
2026-03-17 04:07:15 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, Callb
 # ------------------------
 # Configuration & Globals
 # ------------------------
-TOKEN = os.getenv("TELEGRAM_TOKEN")
+TOKEN = os.getenv("7720733633:AAEmRzuV9r7sdOXeVSQ-4rZ98mo3SW0yb20")
 
 task_queue = asyncio.Queue()
 current_task = None
